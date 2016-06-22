4bd1b746b1b33102b54c50148016d3f7f6caec36
Christian Grabowski <cpg1111@users.noreply.github.com>
2016-06-22 01:05:44 -0400

    project REST interface (#7)

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -5,7 +5,7 @@ type ResultCallback func(interface{}, error)
 type NoResultCallback func(error)
 
 type Datastore interface {
-	Save(key string, data interface{}, callback ResultCallback)
+	Save(key string, data interface{}, callback NoResultCallback)
 	Find(queryStr string, callback ResultCallback)
 	Remove(queryStr string, callback NoResultCallback)
 	Update(queryStr string, update interface{}, callback NoResultCallback)
--- main.go
+++ main.go
@@ -3,20 +3,20 @@ package main
 import (
 	"flag"
 	"log"
-    "os"
+	"os"
 
 	"github.com/cpg1111/maestrod/config"
-    "github.com/cpg1111/maestrod/datastore"
+	"github.com/cpg1111/maestrod/datastore"
 	"github.com/cpg1111/maestrod/server"
 )
 
 var (
-	configPath   = flag.String("config-path", "/etc/maestrod/conf.toml", "path to the config file to load, defaults to /etc/maestrod/conf.toml")
-	runtime      = flag.String("runtime", "", "type of runtime, defaults to native or the value specifcied in the configuration file, other options are: docker, kubernetes, rkt, EC2, GCE, DO, libvirt")
-	hostIP       = flag.String("host-ip", "", "host ip for the server to bind to, defaults to 127.0.0.1 or the value specifcied in the configuration file")
-	port         = flag.Uint("port", 0, "port number for the server to listen on, defaults to 8484 or the value specifcied in the configuration file")
-	workspaceDir = flag.String("workspace-dir", "", "working directory for maestro cloning and building, defaults to /tmp/maestro")
-    datastoreType := flag.String("datastore-type", "redis", "type of data store to persist configuration in, defaults to redis")
+	configPath    = flag.String("config-path", "/etc/maestrod/conf.toml", "path to the config file to load, defaults to /etc/maestrod/conf.toml")
+	runtime       = flag.String("runtime", "", "type of runtime, defaults to native or the value specifcied in the configuration file, other options are: docker, kubernetes, rkt, EC2, GCE, DO, libvirt")
+	hostIP        = flag.String("host-ip", "", "host ip for the server to bind to, defaults to 127.0.0.1 or the value specifcied in the configuration file")
+	port          = flag.Uint("port", 0, "port number for the server to listen on, defaults to 8484 or the value specifcied in the configuration file")
+	workspaceDir  = flag.String("workspace-dir", "", "working directory for maestro cloning and building, defaults to /tmp/maestro")
+	datastoreType = flag.String("datastore-type", "redis", "type of data store to persist configuration in, defaults to redis")
 )
 
 func main() {
@@ -28,33 +28,34 @@ func main() {
 	if *hostIP != "" {
 		conf.Server.Host = *hostIP
 	}
-	if conf.Server.Port != 0 {
+	if *port != 0 {
 		conf.Server.Port = *port
 	}
-	if conf.Server.WorkspaceDir != "" {
+	if *workspaceDir != "" {
 		conf.Server.WorkspaceDir = *workspaceDir
 	}
-    if conf.Server.DataStoreType != "" {
-        conf.Server.DataStoreType = *datastoreType
-    }
-    var datastoreHost string
-    var datastorePort string
-    if conf.Server.DataStoreEnvIP != "" {
-        datastoreHost = os.Getenv(conf.Server.DataStoreEnvIP)
-    } else {
-        datastoreHost = conf.Server.DataStoreStaticIP
-    }
-    if conf.Server.DataStoreEnvPort != "" {
-        datastorePort = os.Getenv(conf.Server.DataStoreEnvPort)
-    } else {
-        datastorePort = conf.Server.DataStoreStaticPort
-    }
-    var store datastore.Datastore
-    switch conf.Server.DataStoreType {
-    case "redis":
-        store = datastore.NewRedis(datastoreHost, datastorePort, conf.Server.DataStorePWD)
-    default:
-        log.Fatal("specifcied datastore currently not supported, please create an issue @ https://github.com/cpg1111/maestrod")
-    }
-	server.Run(&conf.Server, store)
+	if *datastoreType != "" {
+		conf.Server.DataStoreType = *datastoreType
+	}
+	var datastoreHost string
+	var datastorePort string
+	if conf.Server.DataStoreEnvIP != "" {
+		datastoreHost = os.Getenv(conf.Server.DataStoreEnvIP)
+	} else {
+		datastoreHost = conf.Server.DataStoreStaticIP
+	}
+	if conf.Server.DataStoreEnvPort != "" {
+		datastorePort = os.Getenv(conf.Server.DataStoreEnvPort)
+	} else {
+		datastorePort = conf.Server.DataStoreStaticPort
+	}
+	var store datastore.Datastore
+	switch conf.Server.DataStoreType {
+	case "redis":
+		store = datastore.NewRedis(datastoreHost, datastorePort, conf.Server.DataStorePWD)
+		log.Println("redis datastore created")
+	default:
+		log.Fatal("specifcied datastore currently not supported, please create an issue @ https://github.com/cpg1111/maestrod")
+	}
+	server.Run(&conf.Server, &store)
 }
