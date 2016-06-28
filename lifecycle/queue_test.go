27edb2b2c4cb267ec2e65ac6f8aa0898bae0acc7
Christian Grabowski <cpg1111@users.noreply.github.com>
2016-06-27 21:35:14 -0400

    Project lifecycle (#9)
    * project handler update
    
    * lifecycle check
    
    * add queue tests
    
    * add missing file

---
+++ lifecycle/queue_test.go
@@ -0,0 +1,34 @@
+package lifecycle
+
+import (
+	"os"
+	"testing"
+
+	"github.com/cpg1111/maestrod/datastore"
+)
+
+var store = datastore.NewRedis(os.Getenv("REDIS_SERVICE_HOST"), os.Getenv("REDIS_SERVICE_PORT"), "")
+
+var castedStore = (datastore.Datastore)(*store)
+
+var queue = NewQueue(&castedStore)
+
+func TestAdd(t *testing.T) {
+	queue.Add("test", "test", "asdfasdfasdf")
+	if !(queue.Queue[0].Project == "test" && queue.Queue[0].Branch == "test" && queue.Queue[0].Commit == "asdfasdfasdf") {
+		t.Error("Add did not add all the fields")
+	}
+}
+
+func TestPop(t *testing.T) {
+	queue.Add("test", "test", "asdfasdfasdf")
+	running := &Running{}
+	next := queue.Pop(running, 1)
+	if next == nil {
+		t.Error("queue should've popped an entry")
+	}
+	another := queue.Pop(running, 1)
+	if another != nil {
+		t.Error("queue's second pop should've returned nil")
+	}
+}
