8613f3a6c9573ac5fbc703db5ad3b9899e210a97
Christian Grabowski <christiang11754@gmail.com>
2016-08-15 00:30:45 -0400

    fix test scripts

--- etcd_up.sh
+++ etcd_up.sh
@@ -12,12 +12,15 @@ go get -u github.com/coreos/etcd
 
 cd $GOPATH/src/github.com/coreos/etcd
 
+PORT_PREFIX=''
+
 if [ ! -z "$1" ]; then
     git checkout $1
+    PORT_PREFIX=2
 else
     git checkout d53923c636e0e4ab7f00cb75681b97a8f11f5a9d
+    PORT_PREFIX=3
 fi
 
 ./build
-goreman start
-
+./bin/etcd --listen-peer-urls="0.0.0.0:`echo $PORT_PREFIX`2379,0.0.0.0:`echo $PORT_PREFIX`4001" --listen-client-urls="0.0.0.0:`echo $PORT_PREFIX`2380,0.0.0.0:`echo $PORT_PREFIX`7001"
