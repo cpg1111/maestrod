4544f1fd89372540d6045c3ed1bc33dbacf0124a
Christian Grabowski <christiang11754@gmail.com>
2016-09-06 22:16:53 -0400

    statecom handler get functions

--- statecom/handler.go
+++ statecom/handler.go
@@ -2,6 +2,7 @@ package statecom
 
 import (
 	"net/http"
+	"net/url"
 
 	"github.com/cpg1111/maestrod/datastore"
 )
@@ -40,5 +41,54 @@ func (h *Handler) Get(res http.ResponseWriter, req *http.Request) {
 		h.getAll(res, req)
 	} else {
 		h.getOne(res, req, query, project)
+	}
+}
+
+func (h *Handler) getAll(res http.ResponseWriter, req *http.Request) {
+	resChan := make(chan []byte)
+	errChan := make(chan error)
+	h.Store.Find("/configs", func(val []byte, err error) {
+		if err != nil {
+			errChan <- err
+			return
+		}
+		resChan <- val
+	})
+	for {
+		select {
+		case dataErr := <-errChan:
+			res.WriteHeader(500)
+			res.Write([]byte("500 Internal Error"))
+			return
+		case dataRes := <-resChan:
+			res.WriteHeader(200)
+			res.Write(val)
+			return
+		}
+	}
+}
+
+func (h *Handler) getOne(res http.ResponseWriter, req *http.Request, query url.Values, project string) {
+	resChan := make(chan []byte)
+	errChan := make(chan error)
+	key := fmt.Sprintf("/configs/%s", project)
+	h.Store.Find(key, func(val []byte, err error) {
+		if err != nil {
+			errChan <- err
+			return
+		}
+		resChan <- val
+	})
+	for {
+		select {
+		case dataErr := <-errChan:
+			res.WriteHeader(500)
+			res.Write([]byte("500 Internal Error"))
+			return
+		case dataRes := <-resChan:
+			res.WriteHeader(200)
+			res.Write(val)
+			return
+		}
 	}
 }
