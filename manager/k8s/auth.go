5a61f27a094f64255baacd36ab47403adf6b3b0c
Christian Grabowski <christiang11754@gmail.com>
2016-09-10 09:37:35 -0400

    move cert loading to more accessible package

--- manager/k8s/auth.go
+++ manager/k8s/auth.go
@@ -7,36 +7,26 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/cpg1111/maestrod/cert"
 	"github.com/cpg1111/maestrod/config"
 )
 
 func loadCerts(conf *config.Server) (*tls.Config, error) {
-	certData, cDataErr := ioutil.ReadFile(conf.ClientCertPath)
-	if cDataErr != nil {
-		return nil, cDataErr
-	}
-	keyData, kDataErr := ioutil.ReadFile(conf.ClientKeyPath)
-	if kDataErr != nil {
-		return nil, kDataErr
-	}
-	cert, certErr := tls.X509KeyPair(certData, keyData)
+	certificate, certErr := cert.GetKeyPair(conf.ClientCertPath, conf.ClientKeyPath)
 	if certErr != nil {
 		return nil, certErr
 	}
-	rootCAPath := os.Getenv("ROOT_CA_PATH")
-	if len(rootCAPath) > 0 {
-		rootCAData, rootCAErr := ioutil.ReadFile(rootCAPath)
-		if rootCAErr != nil {
-			return nil, rootCAErr
-		}
-		certPool := x509.NewCertPool()
-		certPool.AppendCertsFromPEM(rootCAData)
+	certPool, poolErr := cert.GetRootCA()
+	if poolErr != nil {
+		return nil, poolErr
+	}
+	if certPool != nil {
 		return &tls.Config{
-			Certificates: []tls.Certificate{cert},
+			Certificates: []tls.Certificate{certificate},
 			RootCAs:      certPool,
 		}, nil
 	}
-	return &tls.Config{Certificates: []tls.Certificate{cert}}, nil
+	return &tls.Config{Certificates: []tls.Certificate{certificate}}, nil
 }
 
 func NewAuthTransport(conf *config.Server) (*http.Transport, error) {
